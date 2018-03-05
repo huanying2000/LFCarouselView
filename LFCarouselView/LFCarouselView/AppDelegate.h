0eda9462b3aed79a09458e99de4c9f60eb153019
ios开发 <[715416878@qq.com]>
2018-03-05 15:44:03 +0800

    初始化项目

---
+++ LFCarouselView/LFCarouselView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFCarouselView
+//
+//  Created by ios开发 on 2018/3/5.
+//  Copyright © 2018年 ios开发. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
